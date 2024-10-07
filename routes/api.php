1c2533146380659821f8bd135dc5301224c3f88c
sob1rdev <20sobirjon05@gmail.com>
2024-10-07 17:07:54 +0500

    feat: add API endpoints for status, ads, and branches management

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Ad;
+use App\Models\Branch;
+use App\Models\Status;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -8,6 +11,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', function () {
     $users = User::all();
     return response()->json($users);
+});
+
+Route::get('/branches', function () {
+    $branches = Branch::all();
+    return response()->json($branches);
+});
+
+
+Route::get('/ads', function () {
+    $ads = Ad::all();
+    return response()->json($ads);
+});
+
+Route::get('/statuses', function () {
+    $statuses = Status::all();
+    return response()->json($statuses);
 });
 
 Route::get('/user', function (Request $request) {
